6400bafd4110bdf1bc26857bf3c66423d7d8b51c
Ankit Kumar <104667338+CodeAnk2829@users.noreply.github.com>
2022-07-15 16:32:39 +0530

    Input and Output in C language

--- ques6.c
+++ ques6.c
@@ -1,9 +1,6 @@
 //Program to print user's name in the given format ("Hello! Ankit Kumar")
 #include <stdio.h>
 int main(){
-    char str[30];
-    printf("Enter your name\n");
-    gets(str);
-    printf("\"Hello! %s\"", str);
+    printf("\"Hello! , Ankit Kumar\"");
     return 0;
 }
